987cff681f51e58977d3e9889c1adeef35b523fc
Balachandar <crassydeveloper@gmail.com>
2021-11-30 18:49:21 +0530

    User-service basic version added.

---
+++ user-service/src/main/java/com/balu/springboot/userservice/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.balu.springboot.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
